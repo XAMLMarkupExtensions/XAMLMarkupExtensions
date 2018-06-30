0f38208ae6e89a83235de74c6018a7731b652789
Konrad Mattheis <konrad.mattheis@akquinet.de>
2018-06-30 09:47:14 +0200

    move Tuple NET35 Class to sep. file

--- src/Base/NestedMarkupExtension.cs
+++ src/Base/NestedMarkupExtension.cs
@@ -6,8 +6,6 @@
 // <author>Uwe Mayer</author>
 #endregion
 
-using System.Windows.Media;
-
 namespace XAMLMarkupExtensions.Base
 {
     #region Uses
@@ -17,8 +15,8 @@ namespace XAMLMarkupExtensions.Base
     using System.Linq;
     using System.Reflection;
     using System.Windows;
-    using System.Windows.Markup;
-#if NET40
+    using System.Windows.Markup;   
+#if !NET35
     using System.Xaml;
 #endif
     #endregion
@@ -53,101 +51,6 @@ namespace XAMLMarkupExtensions.Base
             this.EndpointValue = null;
         }
     }
-
-#if NET35
-    /// <summary>
-    /// A simple Tuple class for .NET3.5
-    /// <para>From an answer in http://stackoverflow.com/questions/1171812/multi-key-dictionary-in-c</para>
-    /// </summary>
-    /// <typeparam name="T1">The first item type.</typeparam>
-    /// <typeparam name="T2">The second item type.</typeparam>
-    public struct Tuple<T1, T2>
-    {
-        /// <summary>
-        /// Gets the first item.
-        /// </summary>
-        public readonly T1 Item1;
-
-        /// <summary>
-        /// Gets the second item.
-        /// </summary>
-        public readonly T2 Item2;
-
-        /// <summary>
-        /// Creates a new instance.
-        /// </summary>
-        /// <param name="item1">The first item.</param>
-        /// <param name="item2">The second item.</param>
-        public Tuple(T1 item1, T2 item2) { Item1 = item1; Item2 = item2; }
-    }
-
-    /// <summary>
-    /// A simple Tuple class for .NET3.5
-    /// <para>From an answer in http://stackoverflow.com/questions/1171812/multi-key-dictionary-in-c</para>
-    /// </summary>
-    /// <typeparam name="T1">The first item type.</typeparam>
-    /// <typeparam name="T2">The second item type.</typeparam>
-    /// <typeparam name="T3">The third item type.</typeparam>
-    public struct Tuple<T1, T2, T3>
-    {
-        /// <summary>
-        /// Gets the first item.
-        /// </summary>
-        public readonly T1 Item1;
-
-        /// <summary>
-        /// Gets the second item.
-        /// </summary>
-        public readonly T2 Item2;
-
-        /// <summary>
-        /// Gets the third item.
-        /// </summary>
-        public readonly T3 Item3;
-
-        /// <summary>
-        /// Creates a new instance.
-        /// </summary>
-        /// <param name="item1">The first item.</param>
-        /// <param name="item2">The second item.</param>
-        /// <param name="item3">The third item.</param>
-        public Tuple(T1 item1, T2 item2, T3 item3) { Item1 = item1; Item2 = item2; Item3 = item3; }
-    }
-
-    /// <summary>
-    /// A static tuple factory class.
-    /// </summary>
-    public static class Tuple
-    {
-        /// <summary>
-        /// Creates a new tuple.
-        /// </summary>
-        /// <typeparam name="T1">The first item type.</typeparam>
-        /// <typeparam name="T2">The second item type.</typeparam>
-        /// <param name="item1">The first item.</param>
-        /// <param name="item2">The second item.</param>
-        /// <returns>A new tuple.</returns>
-        public static Tuple<T1, T2> Create<T1, T2>(T1 item1, T2 item2)
-        {
-            return new Tuple<T1, T2>(item1, item2);
-        }
-
-        /// <summary>
-        /// Creates a new tuple.
-        /// </summary>
-        /// <typeparam name="T1">The first item type.</typeparam>
-        /// <typeparam name="T2">The second item type.</typeparam>
-        /// <typeparam name="T3">The third item type.</typeparam>
-        /// <param name="item1">The first item.</param>
-        /// <param name="item2">The second item.</param>
-        /// <param name="item3">The third item.</param>
-        /// <returns>A new tuple.</returns>
-        public static Tuple<T1, T2, T3> Create<T1, T2, T3>(T1 item1, T2 item2, T3 item3)
-        {
-            return new Tuple<T1, T2, T3>(item1, item2, item3);
-        }
-    }
-#endif
 
     /// <summary>
     /// This class walks up the tree of markup extensions to support nesting.
@@ -285,7 +188,7 @@ namespace XAMLMarkupExtensions.Base
 
 #if NET35
             rootObjectHashCode = 0;
-#elif NET40
+#else
             // Try to cast the passed serviceProvider to a IRootObjectProvider and if the cast fails return null
             IRootObjectProvider rootObject = serviceProvider.GetService(typeof(IRootObjectProvider)) as IRootObjectProvider;
             if (rootObject == null)
@@ -723,7 +626,7 @@ namespace XAMLMarkupExtensions.Base
                 }
             }
 
-#if NET40
+#if !NET35
             /// <summary>
             /// Clears the listeners list for the given root object hash code <paramref name="rootObjectHashCode"/>.
             /// </summary>
