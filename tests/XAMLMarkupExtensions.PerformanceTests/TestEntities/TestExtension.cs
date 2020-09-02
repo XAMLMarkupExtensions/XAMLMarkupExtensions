067ed30289b98d768b191d5660974f17c2feeea7
all.owing <all.owing@yandex.ru>
2020-09-02 22:20:38 +0500

    Created performance tests

---
+++ tests/XAMLMarkupExtensions.PerformanceTests/TestEntities/TestExtension.cs
@@ -0,0 +1,24 @@
+﻿namespace XAMLMarkupExtensions.PerformanceTests.TestEntities
+{
+    #region Uses
+    using Base;
+    #endregion
+
+    /// <summary>
+    /// Test extension which implements NestedMarkupExtension.
+    /// </summary>
+    public class TestExtension : NestedMarkupExtension
+    {
+        /// <inheritdoc />
+        public override object FormatOutput(TargetInfo endPoint, TargetInfo info)
+        {
+            return string.Empty;
+        }
+
+        /// <inheritdoc />
+        protected override bool UpdateOnEndpoint(TargetInfo endpoint)
+        {
+            return true;
+        }
+    }
+}
